5ff637fe79bcd13b980f953a11874cea27fc41db
siwonKH <kanghyunkim313@gmail.com>
2023-10-19 10:05:11 +0900

    Add api tests

--- bus_boarding_api/tests/test_bus_boarding_api.py
+++ bus_boarding_api/tests/test_bus_boarding_api.py
@@ -1,7 +1,11 @@
 import pytest
 from fastapi import FastAPI
 from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
+
+from bus_boarding_api.db.dao.user_dao import UserDAO
+from bus_boarding_api.authentication import create_access_token
 
 
 @pytest.mark.anyio
@@ -15,3 +19,38 @@ async def test_health(client: AsyncClient, fastapi_app: FastAPI) -> None:
     url = fastapi_app.url_path_for("health_check")
     response = await client.get(url)
     assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.anyio
+async def test_create_user(
+    fastapi_app: FastAPI,
+    client: AsyncClient,
+    dbsession: AsyncSession,
+) -> None:
+    url = fastapi_app.url_path_for("create_user")
+    new_user = {
+        "student_id": "3101",
+        "name": "홍길동",
+        "phone_number": "01012345678",
+        "year": 2023,
+    }
+
+    headers = {"Content-Type": "application/json"}
+    response = await client.post(url, json=new_user, headers=headers)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    user_dao = UserDAO(dbsession)
+    await user_dao.create_super_user()
+    access_token = create_access_token('1')
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}",
+    }
+    response = await client.post(url, json=new_user, headers=headers)
+    assert response.status_code == status.HTTP_200_OK
+
+    saved_new_user = await user_dao.get(2)
+    assert saved_new_user.name == new_user.get("name")
+
+
+
