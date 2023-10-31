79a0d385fd102d0525f81aa1224f23302aeb77d7
siwonKH <kanghyunkim313@gmail.com>
2023-10-31 22:07:03 +0900

    station to stops

--- bus_boarding_api/web/api/router.py
+++ bus_boarding_api/web/api/router.py
@@ -6,7 +6,7 @@ api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
 api_router.include_router(boarding_record.router, prefix="/boarding", tags=["boarding"])
-api_router.include_router(bus_stop.router, prefix="/station", tags=["station"]),
+api_router.include_router(bus_stop.router, prefix="/stops", tags=["station"]),
 api_router.include_router(bus.router, prefix="/bus", tags=["bus"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
