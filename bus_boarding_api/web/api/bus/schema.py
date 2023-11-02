0337d65a14facfed9a82cfda4591d51ad9b42cb4
siwonKH <kanghyunkim313@gmail.com>
2023-11-02 17:26:24 +0900

    updated user router

--- bus_boarding_api/web/api/bus/schema.py
+++ bus_boarding_api/web/api/bus/schema.py
@@ -1,9 +1,14 @@
-from typing import List
+from datetime import datetime
 
 from pydantic import BaseModel, ConfigDict
 
-from bus_boarding_api.web.api.bus_stop.schema import BusStopModelDTO
-from bus_boarding_api.web.api.boarding_record.schema import BoardingRecordModelDTO
+
+class BoardingRecordModelDTO(BaseModel):
+    id: int
+    user_id: int
+    boarding_bus_id: int
+    destination_stop_id: int
+    time_created: datetime
 
 
 class BusModelDTO(BaseModel):
@@ -15,27 +20,25 @@ class BusModelDTO(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
 
-class BusModelWithAllDTO(BaseModel):
-    id: int
-    name: str
-    destination: int
-    description: str
-
-    bus_stops: List[BusStopModelDTO]
-    boarding_records: List[BoardingRecordModelDTO]
-
-    # model_config = ConfigDict(from_attributes=True)
+# class BusModelWithAllDTO(BaseModel):
+#     id: int
+#     name: str
+#     destination: int
+#     description: str
+#
+#     bus_stops: List[BusStopModelDTO]
+#     boarding_records: List[BoardingRecordModelDTO]
 
 
-class BusModelWithStopsDTO(BaseModel):
-    id: int
-    name: str
-    destination: int
-    description: str
-
-    bus_stop: BusStopModelDTO
-
-    model_config = ConfigDict(from_attributes=True)
+# class BusModelWithStopsDTO(BaseModel):
+#     id: int
+#     name: str
+#     destination: int
+#     description: str
+#
+#     bus_stop: BusStopModelDTO
+#
+#     model_config = ConfigDict(from_attributes=True)
 
 
 class BusModelWithRecordsDTO(BaseModel):
@@ -53,3 +56,11 @@ class BusModelInputDTO(BaseModel):
     name: str
     destination: int
     description: str
+
+
+class UserModelDTO(BaseModel):
+    id: int
+    student_id: str
+    name: str
+    phone_number: str
+    role: str
--- bus_boarding_api/web/api/bus/views.py
+++ bus_boarding_api/web/api/bus/views.py
@@ -9,40 +9,49 @@ from bus_boarding_api.db.dao.user_dao import UserDAO
 from bus_boarding_api.db.models.boarding_record import BoardingRecordModel
 from bus_boarding_api.db.models.bus import BusModel
 from bus_boarding_api.db.models.user import UserModel
-from bus_boarding_api.web.api.bus.schema import BusModelDTO, BusModelInputDTO, BusModelWithAllDTO, BusModelWithStopsDTO, BusModelWithRecordsDTO
+from bus_boarding_api.web.api.bus.schema import (BusModelDTO, BusModelInputDTO,
+                                                 UserModelDTO, BoardingRecordModelDTO)
 from bus_boarding_api.authentication import PermissionChecker
 from bus_boarding_api.permissions.models_permissions import User, Bus, BusStop, BoardingRecord
 
 router = APIRouter()
 
 
+# @router.get("/{bus_id}",
+#             dependencies=[Depends(PermissionChecker([Bus.permissions.READ, BusStop.permissions.READ, BoardingRecord.permissions.READ]))],
+#             response_model=BusModelWithAllDTO)
+# async def get_bus(bus_id: int, bus_dao: BusDAO = Depends()) -> BusModel:
+#     fucking_bus_data = await bus_dao.get(
+#         bus_id=bus_id,
+#         load_stops=True,
+#         load_boarding_records=True,
+#     )
+#     print(fucking_bus_data.boarding_records)
+#     print(fucking_bus_data.bus_stops[0])
+#     return fucking_bus_data
+
+
 @router.get("/{bus_id}",
             dependencies=[Depends(PermissionChecker([Bus.permissions.READ, BusStop.permissions.READ, BoardingRecord.permissions.READ]))],
-            response_model=BusModelWithAllDTO)
+            response_model=BusModelDTO)
 async def get_bus(bus_id: int, bus_dao: BusDAO = Depends()) -> BusModel:
-    fucking_bus_data = await bus_dao.get(
-        bus_id=bus_id,
-        load_stops=True,
-        load_boarding_records=True,
-    )
-    print(fucking_bus_data.boarding_records)
-    print(fucking_bus_data.bus_stops[0])
-    return fucking_bus_data
+    bus = await bus_dao.get(bus_id=bus_id)
+    return bus
 
 
-@router.get("/{bus_id}/stops",
-            dependencies=[Depends(PermissionChecker([Bus.permissions.READ, BusStop.permissions.READ]))],
-            response_model=BusModelWithStopsDTO)
-async def get_bus_stops(bus_id: int, bus_dao: BusDAO = Depends()) -> BusModel:
-    return await bus_dao.get(
-        bus_id=bus_id,
-        load_stops=True,
-    )
+# @router.get("/{bus_id}/stops",
+#             dependencies=[Depends(PermissionChecker([Bus.permissions.READ, BusStop.permissions.READ]))],
+#             response_model=BusModelWithStopsDTO)
+# async def get_bus_stops(bus_id: int, bus_dao: BusDAO = Depends()) -> BusModel:
+#     return await bus_dao.get(
+#         bus_id=bus_id,
+#         load_stops=True,
+#     )
 
 
 @router.get("/{bus_id}/records",
             dependencies=[Depends(PermissionChecker([Bus.permissions.READ, BoardingRecord.permissions.READ]))],
-            response_model=BusModelWithRecordsDTO)
+            response_model=List[BoardingRecordModelDTO])
 async def get_bus_records(bus_id: int, boarding_record_dao: BoardingRecordDAO = Depends()) -> List[BoardingRecordModel]:
     return await boarding_record_dao.get_today_record_by_bus_id(
         bus_id=bus_id,
@@ -52,7 +61,7 @@ async def get_bus_records(bus_id: int, boarding_record_dao: BoardingRecordDAO = 
 @router.get("/{bus_id}/users",
             dependencies=[Depends(PermissionChecker(
                 [Bus.permissions.READ, User.permissions.READ]))],
-            response_model=BusModelWithRecordsDTO)
+            response_model=List[UserModelDTO])
 async def get_bus_users(bus_id: int, user_dao: UserDAO = Depends()) -> List[UserModel]:
     return await user_dao.get_all_by_bus_id(bus_id=bus_id)
 
