064807a1852cce38b5ab811be1ba20a41077ed48
siwonKH <kanghyunkim313@gmail.com>
2023-11-14 19:26:49 +0900

    first try to fix get_db_session issue

--- bus_boarding_api/db/dependencies.py
+++ bus_boarding_api/db/dependencies.py
@@ -1,10 +1,12 @@
 import random
+from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
 
 
+@asynccontextmanager
 async def get_db_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
     session: AsyncSession = request.app.state.db_session_factory()
 
