d9bdd1c1920484592e15d15f8d986f7a8945b7f7
siwonKH <kanghyunkim313@gmail.com>
2023-11-14 17:56:32 +0900

    debug db session closure part

--- bus_boarding_api/db/dependencies.py
+++ bus_boarding_api/db/dependencies.py
@@ -8,10 +8,14 @@ async def get_db_session(request: Request) -> AsyncGenerator[AsyncSession, None]
     session: AsyncSession = request.app.state.db_session_factory()
 
     try:  # noqa: WPS501
+        print('session created')
         yield session
     except Exception as e:
         # logger.error(f"An error occurred: {e}")
+        print(e)
+        print('session error')
         await session.rollback()
         raise
     finally:
+        print('session finally')
         await session.close()
