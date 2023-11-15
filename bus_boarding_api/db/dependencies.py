45e3bfac78d3fcf8dd872e449110974576b2d1ec
siwonKH <kanghyunkim313@gmail.com>
2023-11-15 10:12:58 +0900

    get_db_session bug fix trial

--- bus_boarding_api/db/dependencies.py
+++ bus_boarding_api/db/dependencies.py
@@ -8,14 +8,14 @@ from starlette.requests import Request
 async def get_db_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
     session: AsyncSession = request.app.state.db_session_factory()
 
-    rand = random.randint(1, 100000000)
+    rand = random.randint(10000000, 99999999)
 
     try:  # noqa: WPS501
-        print('session created', rand)
+        print('session created v2', rand)
         yield session
     except Exception:
         await session.rollback()
         raise
     finally:
-        print('session finally', rand)
+        print('session finally v2', rand)
         await session.close()
